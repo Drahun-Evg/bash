8fb593f7cfebe240bb202fe8493afbd05947bdb5
Drahun-Evg <100157401+Drahun-Evg@users.noreply.github.com>
2023-02-21 20:35:53 +0300

    Create minikube.sh

---
+++ minikube.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+sudo dpkg -i minikube_latest_amd64.deb
+
+minikube version
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install kubectl -y
+
+kubectl version --client
+
+# minikube start
+# kubectl cluster-info
+# kubectl get nodes
+# minikube stop
