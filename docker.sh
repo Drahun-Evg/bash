aee6608624041ed9f3255d9a3a9240c0fc62ebd8
Drahun-Evg <100157401+Drahun-Evg@users.noreply.github.com>
2023-02-21 20:43:05 +0300

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get install curl -y
+sudo curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo groupadd docker
+sudo gpasswd -a $USER docker
+newgrp docker
