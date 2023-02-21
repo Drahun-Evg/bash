4c8b3b039c22e89d5d0806336e6c560f0f675826
Drahun-Evg <100157401+Drahun-Evg@users.noreply.github.com>
2023-02-21 20:33:28 +0300

    Create Jenkins.sh

---
+++ Jenkins.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Debian's OS
+
+sudo apt update
+
+#1) JAVA INSTALL
+
+sudo apt install openjdk-11-jdk -y
+java --version
+
+
+#2) JENKINS INSTALL
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
+jenkins --version
